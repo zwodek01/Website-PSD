a1487143f1b3bb3150e4438367f92f98e7740041
Mateusz Zawadzki <zwodek01@gmail.com>
2019-08-04 21:13:51 +0200

    Contact Section Completed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,23 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const barsIcon = document.querySelector(".fa-bars");
+const list = document.querySelector(".navigation__list");
+const link = document.querySelectorAll(".navigation__list-link");
+
+barsIcon.addEventListener('click', () => {
+    list.classList.toggle('hide');
+})
+
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', () => {
+        list.classList.add('hide');
+    })
+}
+
+// console.log(window.pageYOffset)
+
+// function getScroll {
+//     console.log(window.pageYOffset);
+// }
+
+// getScroll();
